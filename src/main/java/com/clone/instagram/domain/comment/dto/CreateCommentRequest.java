7e5d4d7c7e2057b746f28f74d6b5f064d915eea3
daeuun <leeda51925@gmail.com>
2023-05-09 23:17:10 +0900

    Refactor: Replace default constructor with @NoArgsConstructor

--- src/main/java/com/clone/instagram/domain/comment/dto/CreateCommentRequest.java
+++ src/main/java/com/clone/instagram/domain/comment/dto/CreateCommentRequest.java
@@ -3,18 +3,19 @@ package com.clone.instagram.domain.comment.dto;
 import com.clone.instagram.domain.comment.model.Comment;
 import com.clone.instagram.domain.post.model.Posts;
 import com.clone.instagram.domain.user.model.Users;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateCommentRequest {
-    String content;
-    Long userId;
-    Long postId;
-    Long originalCommentId;
-
-    public CreateCommentRequest(){}
+    public String content;
+    public Long userId;
+    public Long postId;
+    public Long originalCommentId;
 
     public Comment toEntity(Users user, Posts post, Comment originalComment) {
         return new Comment(this.content, user, post, originalComment);
